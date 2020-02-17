16ace206cb7aeaa616efccd1f5162c8034914cb9
h0341a <qianaiwuyu@gmail.com>
2020-02-17 21:59:51 +0800

    simple use of swagger2 to generate API documents.

--- api/src/main/java/com/ccjr/api/HelloController.java
+++ api/src/main/java/com/ccjr/api/HelloController.java
@@ -1,5 +1,8 @@
 package com.ccjr.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ling
  */
 @RestController
+@Api(tags = "hello")
 public class HelloController {
-    @RequestMapping("/hello")
+    @ApiOperation("print hello")
+    @GetMapping("/hello")
     public String hello() {
         return "hello, the multi module project.";
     }
