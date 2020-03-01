bd867c25a71ea2feea37cadc682dc18255ba41e2
h0341a <qianaiwuyu@gmail.com>
2020-03-01 16:25:46 +0800

    Add comment DTO

---
+++ model/src/main/java/com/ccjr/model/dto/CommentDto.java
@@ -0,0 +1,18 @@
+package com.ccjr.model.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class CommentDto {
+
+    @NotBlank(message = "昵称不能为空")
+    private String nickName;
+    @Email
+    private String email;
+    @NotBlank(message = "内容不能为空")
+    private String content;
+
+}
