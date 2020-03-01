a8d1839b0325b651c3d5d7a85b414405912c9bc9
h0341a <qianaiwuyu@gmail.com>
2020-03-01 16:26:19 +0800

    alter name

--- model/src/main/java/com/ccjr/model/dto/CommentDTO.java
+++ model/src/main/java/com/ccjr/model/dto/CommentDTO.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
-public class CommentDto {
+public class CommentDTO {
 
     @NotBlank(message = "昵称不能为空")
     private String nickName;
