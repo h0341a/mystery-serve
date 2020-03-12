1e348d4cc2a8fcbfdedf7cfc72c37a5989728abd
h0341a <qianaiwuyu@gmail.com>
2020-03-12 16:44:54 +0800

    Open cross domain request

---
+++ startup/src/main/java/com/ccjr/CrosConfiguration.java
@@ -0,0 +1,17 @@
+package com.ccjr;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrosConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .allowedOrigins("*");
+    }
+}
